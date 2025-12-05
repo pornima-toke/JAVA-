f02f4919b896b9b85f14eee7137899a9e4ef2d7e
pornima-toke <pornimatoke045@gmail.com>
2025-12-05 22:13:57 +0530

    add

--- cls&obj/NumberChecker.java
+++ cls&obj/NumberChecker.java
@@ -3,3 +3,27 @@ Create a class NumberChecker with a method isEven that checks if a number is eve
 Explanation: Focuses on using modulus operator in logic.
 */
 class NumberChecker
+{
+	void isEven(int num)
+	{
+		//condition check 
+		if (num % 2 == 0)
+		{
+			System.out.println(num + "is even");
+		}
+		else
+		{
+			System.out.println(num + "is odd");
+		}
+	}
+	// Main method
+	public  static void main(String x[])
+	{
+		//create object
+		NumberChecker obj = new NumberChecker();
+		 //test value
+		 int number = 3;
+		//call method
+		obj.isEven(number);
+	}
+}
